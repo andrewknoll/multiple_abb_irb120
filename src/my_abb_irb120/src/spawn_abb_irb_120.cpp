e239bd19d57657151cacfee40092aac097677696
Andrés <aog25.2014@gmail.com>
2021-07-06 23:00:11 +0200

    Namespace C++ integration

--- src/my_abb_irb120/src/spawn_abb_irb_120.cpp
+++ src/my_abb_irb120/src/spawn_abb_irb_120.cpp
@@ -35,6 +35,16 @@
 /* Author: Sachin Chitta, Dave Coleman, Mike Lautman */
 // MODIFIED BY: Andrés Otero García, Gonzalo López Nicolás and María del Rosario Aragüés Muñoz
 
+
+#include <moveit/robot_model_loader/robot_model_loader.h>
+#include <moveit/planning_interface/planning_interface.h>
+#include <moveit/planning_scene/planning_scene.h>
+#include <moveit/planning_scene_monitor/planning_scene_monitor.h>
+#include <moveit/kinematic_constraints/utils.h>
+#include <moveit_msgs/DisplayTrajectory.h>
+#include <moveit_msgs/PlanningScene.h>
+#include <moveit_visual_tools/moveit_visual_tools.h>
+
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
 
@@ -66,10 +76,13 @@ void printCurrentPose(moveit::planning_interface::MoveGroupInterface& move_group
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "move_group_interface_tutorial");
-  ros::NodeHandle node_handle;
+
+  std::string name_ = "motion_planning_tutorial";
+  ros::init(argc, argv, name_);
+  ros::NodeHandle node_handle("/robot1");
   ros::AsyncSpinner spinner(1);
   spinner.start();
+std::cout << "CP1" << std::endl;
 
   // BEGIN_TUTORIAL
   //
@@ -79,27 +92,29 @@ int main(int argc, char** argv)
   // MoveIt operates on sets of joints called "planning groups" and stores them in an object called
   // the `JointModelGroup`. Throughout MoveIt the terms "planning group" and "joint model group"
   // are used interchangably.
-  static const std::string PLANNING_GROUP = "manipulator";
+  static const std::string PLANNING_GROUP = "robot1/manipulator";
+
+  moveit::planning_interface::MoveGroupInterface::Options opt(PLANNING_GROUP, "/robot1/robot_description", node_handle);
 
   // The :move_group_interface:`MoveGroupInterface` class can be easily
   // setup using just the name of the planning group you would like to control and plan for.
-  moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
-
+  moveit::planning_interface::MoveGroupInterface move_group(opt);
+std::cout << "CP2" << std::endl;
   // We will use the :planning_scene_interface:`PlanningSceneInterface`
   // class to add and remove collision objects in our "virtual world" scene
-  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
-
+  moveit::planning_interface::PlanningSceneInterface planning_scene_interface("/robot1");
+std::cout << "CP3" << std::endl;
   // Raw pointers are frequently used to refer to the planning group for improved performance.
   const robot_state::JointModelGroup* joint_model_group =
       move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
-
+std::cout << "CP4" << std::endl;
   // Visualization
   // ^^^^^^^^^^^^^
   //
   // The package MoveItVisualTools provides many capabilties for visualizing objects, robots,
   // and trajectories in RViz as well as debugging tools such as step-by-step introspection of a script
   namespace rvt = rviz_visual_tools;
-  moveit_visual_tools::MoveItVisualTools visual_tools("base_link");
+  moveit_visual_tools::MoveItVisualTools visual_tools("robot1/base_link");
   visual_tools.deleteAllMarkers();
 
   // Remote control is an introspection tool that allows users to step through a high level script
@@ -140,8 +155,8 @@ int main(int argc, char** argv)
     y: 0.707107
     z: 0
     w: 0.707107
-
-  */
+*/
+  
   printCurrentPose(move_group);
 
   //move_group.setPositionTarget
@@ -204,4 +219,57 @@ int main(int argc, char** argv)
 
   ros::shutdown();
   return 0;
+
+/*
+const std::string PLANNING_GROUP = "robot1/manipulator";
+  robot_model_loader::RobotModelLoader robot_model_loader("robot1/robot_description");
+  robot_model::RobotModelPtr robot_model = robot_model_loader.getModel();
+  // Create a RobotState and JointModelGroup to keep track of the current robot pose and planning group
+  robot_state::RobotStatePtr robot_state(new robot_state::RobotState(robot_model));
+  const robot_state::JointModelGroup* joint_model_group = robot_state->getJointModelGroup(PLANNING_GROUP);
+
+  // Using the :moveit_core:`RobotModel`, we can construct a :planning_scene:`PlanningScene`
+  // that maintains the state of the world (including the robot).
+  planning_scene::PlanningScenePtr planning_scene(new planning_scene::PlanningScene(robot_model));
+
+  // Configure a valid robot state
+  planning_scene->getCurrentStateNonConst().setToDefaultValues(joint_model_group, "ready");
+
+  // We will now construct a loader to load a planner, by name.
+  // Note that we are using the ROS pluginlib library here.
+  boost::scoped_ptr<pluginlib::ClassLoader<planning_interface::PlannerManager>> planner_plugin_loader;
+  planning_interface::PlannerManagerPtr planner_instance;
+  std::string planner_plugin_name;
+
+  // We will get the name of planning plugin we want to load
+  // from the ROS parameter server, and then load the planner
+  // making sure to catch all exceptions.
+  if (!node_handle.getParam("/robot1/move_group/planning_plugin", planner_plugin_name))
+    ROS_FATAL_STREAM("Could not find planner plugin name");
+  try
+  {
+    planner_plugin_loader.reset(new pluginlib::ClassLoader<planning_interface::PlannerManager>(
+        "moveit_core", "planning_interface::PlannerManager"));
+  }
+  catch (pluginlib::PluginlibException& ex)
+  {
+    ROS_FATAL_STREAM("Exception while creating planning plugin loader " << ex.what());
+  }
+  try
+  {
+    planner_instance.reset(planner_plugin_loader->createUnmanagedInstance(planner_plugin_name));
+    if (!planner_instance->initialize(robot_model, node_handle.getNamespace()))
+      ROS_FATAL_STREAM("Could not initialize planner instance");
+    ROS_INFO_STREAM("Using planning interface '" << planner_instance->getDescription() << "'");
+  }
+  catch (pluginlib::PluginlibException& ex)
+  {
+    const std::vector<std::string>& classes = planner_plugin_loader->getDeclaredClasses();
+    std::stringstream ss;
+    for (std::size_t i = 0; i < classes.size(); ++i)
+      ss << classes[i] << " ";
+    ROS_ERROR_STREAM("Exception while loading planner '" << planner_plugin_name << "': " << ex.what() << std::endl
+                                                         << "Available plugins: " << ss.str());
+  }
+*/
 }
