7b5416e9034571793cc93744e4717cdb4ef045ce
Andrés <aog25.2014@gmail.com>
2021-08-18 12:05:04 +0200

    Colors + Parameters

--- src/multiple_abb_irb120/src/WorldPlugin.cpp
+++ src/multiple_abb_irb120/src/WorldPlugin.cpp
@@ -18,16 +18,29 @@ double calculateInitialComponent(int index, double offset, double size, int reso
 }
 
 gazebo::msgs::Pose* calculateInitialPos(int indices[3], double offset[3], double size[3], int resolution[3]){
-    gazebo::msgs::Pose* pos = new gazebo::msgs::Pose();
-    gazebo::msgs::Vector3d* position = new gazebo::msgs::Vector3d();
-    position->set_x(calculateInitialComponent(indices[0], offset[0], size[0], resolution[0]));
-    position->set_y(calculateInitialComponent(indices[1], offset[1], size[1], resolution[1]));
-    position->set_z(calculateInitialComponent(indices[2], offset[2], size[2], resolution[2]));
-    gazebo::msgs::Quaternion* orientation = new gazebo::msgs::Quaternion();
-    pos->set_allocated_position(position);
-    pos->set_allocated_orientation(orientation);
+  gazebo::msgs::Pose* pos = new gazebo::msgs::Pose();
+  gazebo::msgs::Vector3d* position = new gazebo::msgs::Vector3d();
+  position->set_x(calculateInitialComponent(indices[0], offset[0], size[0], resolution[0]));
+  position->set_y(calculateInitialComponent(indices[1], offset[1], size[1], resolution[1]));
+  position->set_z(calculateInitialComponent(indices[2], offset[2], size[2], resolution[2]));
+  gazebo::msgs::Quaternion* orientation = new gazebo::msgs::Quaternion();
+  pos->set_allocated_position(position);
+  pos->set_allocated_orientation(orientation);
 
-    return pos;
+  return pos;
+}
+
+gazebo::msgs::Material* calculateMaterial(int indices[3], int resolution[3]){
+    gazebo::msgs::Material* material = new gazebo::msgs::Material();
+    gazebo::msgs::Color* color = new gazebo::msgs::Color();
+    
+    color->set_r((resolution[1] - indices[1]) / (double)resolution[1]);
+    color->set_g(indices[0] / (double)resolution[0]);
+    color->set_b((resolution[0] - indices[0]) / (double)resolution[0]);;
+    color->set_a(1.0);
+    std::cout << "color: " << color->r() << " " << color->g() << " " << color->b() << " " << color->a() << std::endl;
+    material->set_allocated_ambient(color);
+    return material;
 }
 
 std::string fromFileToString(std::string filename) {
@@ -46,7 +59,8 @@ class Factory : public WorldPlugin
   float width = 5.0, height = 5.0;
   int vertical_res = 10, horizontal_res = 10;
   float offset_x = 0.0, offset_y = 0.0, offset_z = 1.0;
-  float mass = 0.1;
+  float mass = 0.1, stiffness = 20.0, damping = 2.0;
+
 
   public: void Load(physics::WorldPtr _parent, sdf::ElementPtr _sdf)
   {
@@ -60,6 +74,17 @@ class Factory : public WorldPlugin
     if(parameters.count("offset_y") != 0) offset_y = parameters["offset_y"];
     if(parameters.count("offset_z") != 0) offset_z = parameters["offset_z"];
     if(parameters.count("mass") != 0) mass = parameters["mass"];
+    if(parameters.count("stiffness") != 0) stiffness = parameters["stiffness"];
+    if(parameters.count("damping") != 0) damping = parameters["damping"];
+
+    ros::NodeHandle ros_nh;
+    ros_nh.setParam("/grid/width", width);
+    ros_nh.setParam("/grid/height", height);
+    ros_nh.setParam("/grid/resolution", std::vector<int>({vertical_res, horizontal_res}));
+    ros_nh.setParam("/grid/offset", std::vector<double>({offset_x, offset_y, offset_z}));
+    ros_nh.setParam("/grid/mass", mass);
+    ros_nh.setParam("/grid/stiffness", stiffness);
+    ros_nh.setParam("/grid/damping", damping);
         
     this->world = _parent;
 
@@ -79,14 +104,14 @@ class Factory : public WorldPlugin
 
     double offset[3] = {offset_x, offset_y, offset_z};
     double size[3] = {width, height, 1};
-    int resolution[3] = {vertical_res, horizontal_res, 1};
+    int resolution[3] = {horizontal_res, vertical_res, 1};
     int indices[3] = {0, 0, 0};
 
     //Spawn all links with their respective collision spheres
     for(int i = 0; i < vertical_res; i++){
-      indices[0] = i;
+      indices[1] = i;
       for(int j = 0; j < horizontal_res; j++){
-        indices[1] = j;
+        indices[0] = j;
         printf("Creating link...\n");
         suffix = "_" + std::to_string(i * horizontal_res + j);
 
@@ -99,6 +124,10 @@ class Factory : public WorldPlugin
 
         link->set_name("link" + suffix);
         link->set_allocated_pose(pos);
+
+        gazebo::msgs::Visual* visual = link->mutable_visual(link->visual_size()-1);
+        gazebo::msgs::Material* material = calculateMaterial(indices, resolution);
+        visual->set_allocated_material(material);
       }
     }
 
